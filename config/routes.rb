cbe3efdeb56816f9ac7ef27dfb5c1931175fcfe8
Levi Apolinar <leviam51@gmail.com>
2024-08-27 18:24:38 -0500

    Challenge solved - Models & CRUD - 2 - Rails Task Manager

--- config/routes.rb
+++ config/routes.rb
@@ -7,5 +7,18 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "posts#index"
-  get 'tasks', to: "tasks#tasks"
+  # READ (ALL)
+  get "tasks", to: "tasks#tasks"
+  #NEW
+  get "tasks/new", to: "tasks#new"
+  # READ (ONE)
+  get "tasks/:id", to: "tasks#show", as: :task
+  #CREATE
+  post "tasks", to: "tasks#create"
+  #EDIT
+  get "tasks/:id/edit", to: "tasks#edit"
+  #UPDATE
+  patch "tasks/:id", to: "tasks#update"
+  #DELETE
+  delete "tasks/:id", to: "tasks#destroy"
 end
